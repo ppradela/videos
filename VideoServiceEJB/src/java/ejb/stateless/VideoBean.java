ae4da26f25f34f89f183fda0d822e1f1b8784b10
Przemyslaw Pradela <przemyslaw.pradela@gmail.com>
2020-02-17 12:59:03 +0100

    Dodanie EJB Remote

---
+++ VideoServiceEJB/src/java/ejb/stateless/VideoBean.java
@@ -0,0 +1,10 @@
+package ejb.stateless;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class VideoBean implements VideoBeanRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ VideoServiceEJB/src/java/entities/Video.java
@@ -0,0 +1,104 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "VIDEO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Video.findAll", query = "SELECT v FROM Video v")
+    , @NamedQuery(name = "Video.findById", query = "SELECT v FROM Video v WHERE v.id = :id")
+    , @NamedQuery(name = "Video.findByTitle", query = "SELECT v FROM Video v WHERE v.title = :title")})
+public class Video implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "TITLE")
+    private String title;
+    @Lob
+    @Column(name = "FILE")
+    private byte[] file;
+    @Transient
+    private String fileBase64;
+
+    public Video() {
+    }
+
+    public Video(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    public String getFileBase64() {
+        return fileBase64;
+    }
+
+    public void setFileBase64(String fileBase64) {
+        this.fileBase64 = fileBase64;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Video)) {
+            return false;
+        }
+        Video other = (Video) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Video[ id=" + id + " ]";
+    }
+
+}
